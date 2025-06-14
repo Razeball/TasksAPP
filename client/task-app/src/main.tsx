3a7237dd8574fe547f1d7187ccc156c34bf2059a
Razeball <said5rivera@gmail.com>
2025-06-13 20:19:46 -0500

    fix3

--- client/task-app/src/main.tsx
+++ client/task-app/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import logo from "./assets/Logo.svg";
 import AuthProvider from "./context/AuthContext.tsx";
 import App from "./App.tsx";
 
@@ -9,6 +10,15 @@ createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <AuthProvider>
         <App />
+        <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content items-center p-4">
+          <aside className="grid-flow-col items-center">
+            <img className="size-12" src={logo} alt="Logo" />
+            <p>
+              RzCorp Copyright © {new Date().getFullYear()} - All right reserved
+            </p>
+          </aside>
+          <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end"></nav>
+        </footer>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
